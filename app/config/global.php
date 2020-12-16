cf2a5d6d965952a37446ae88e87e9eefecc4c2b4
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-12-16 14:57:52 -0300

    atualizacao 16/12/2020

---
+++ app/config/global.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE', 'receitas/public/');
+define('UNSET_COUNT', 2);
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'myreceitas');
--- receitas/app/config/global.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASE', 'receitas/receitas/public/');
-define('UNSET_COUNT', 3);
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'myreceitas');
