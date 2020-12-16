7061fa610af36a0886a301583d03a8946524536e
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-12-16 11:36:36 -0300

    atualizacao 16/12/2020

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<?php
-    header('Location: ./receitas/public/index.php');
-    exit;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>teste receitas</p>
+</body>
+</html>
