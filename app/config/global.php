6e6d2eaf1fe71ae9c38d91c201c4421d834c448a
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-12-17 11:47:52 -0300

    atualizacao 17/12/2020

--- app/config/global.php
+++ app/config/global.php
@@ -1,9 +1,9 @@
 <?php
 
-define('BASE', '/projetosDev/');
-define('UNSET_COUNT', 2);
+define('BASE', '/public_html/receitas/');
+define('UNSET_COUNT', 1);
 
-define('DB_HOST', '151.106.96.51');
-define('DB_USER', 'u372636767_caravelas');
-define('DB_PASS', 'Supercaravelas2020');
-define('DB_NAME', 'u372636767_caravelas');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'myreceitas');
