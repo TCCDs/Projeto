7e70c12337899f41e4d21cbc04f251e11a14671b
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-12-17 13:01:22 -0300

    atualizacao 17/12/2020

--- client/index.php
+++ client/index.php
@@ -1,7 +1,11 @@
-<?php
-require_once('..\vendor\autoload.php');
-require_once('..\app\config\global.php');
-require_once('..\app\functions\functions.php');
-
-use app\core\Router;
-(new Router());
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Teste acesso
+</body>
+</html>
